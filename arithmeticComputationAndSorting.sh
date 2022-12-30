517320e0998b97c1d6da117d7db5a1131709ca26
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2022-12-30 20:19:15 +0530

    Added: Store the results in a Dictionary for every Computation

--- arithmeticComputationAndSorting.sh
+++ arithmeticComputationAndSorting.sh
@@ -1,3 +1,5 @@
+declare -A arithmaticOperation
+
 # Taking input from user
 echo "Enter first numbers: "
 read a
@@ -6,5 +8,22 @@ read b
 echo "Enter third numbers: "
 read c
 
-com=$(echo $(( a % b + c )))
-echo "Computation result is: "$com
+# here com is computation
+com1=$(echo $(( a + b * c )))
+echo "Computation 1 result is "$com1
+
+com2=$(echo $(( a * b + c )))
+echo "Computation 2 result is: "$com2
+
+com3=$(echo $(( c + a / b )))
+echo "Computation 3 result is: "$com3
+
+com4=$(echo $(( a % b + c )))
+echo "Computation 4 result is: "$com4
+
+arithmaticOperation[result1]=$com1
+arithmaticOperation[result2]=$com2
+arithmaticOperation[result3]=$com3
+arithmaticOperation[result4]=$com4
+
+echo ${arithmaticOperation[@]}
